a0c23a23a24627311ea65f71074472d74ff0e5b4
Endeavor-for-the-auric <endevor781@gmail.com>
2022-05-30 10:52:19 -0500

    update code

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-function myFunction(y) {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-    y.classList.toggle("change");
-  }
-  
   function startFunction(){
-    document.getElementById("myTopnav").style.marginTop = "1%";
-    document.body.style.backgroundColor = "rgb(217 217 217)"
-    document.getElementById("navbars").style.display = "block";
-    document.getElementById("intro-content").style.display = "none";
-  }
+  document.getElementById("myTopnav").style.marginTop = "1%";
+  document.body.style.backgroundColor = "rgb(217 217 217)"
+  document.getElementById("navbars").style.display = "block";
+  document.getElementById("intro-content").style.display = "none";
+  document.getElementById("content").style.display = "block";
+}
