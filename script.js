c33fd8fed5946c6cd151d36965965401720ea30f
Endeavor-for-the-auric <endevor781@gmail.com>
2022-06-29 07:59:33 -0500

    update

--- script.js
+++
@@ -1,7 +0,0 @@
-  function startFunction(){
-  document.getElementById("myTopnav").style.marginTop = "1%";
-  document.body.style.backgroundColor = "rgb(217 217 217)"
-  document.getElementById("navbars").style.display = "block";
-  document.getElementById("intro-content").style.display = "none";
-  document.getElementById("content").style.display = "block";
-}
